ca8aa026a76c72ed8a5e4028de3d5fbe718e0384
DarkadowWolf <girelagarcia.javier29@gmail.com>
2015-11-27 20:22:28 +0100

    [1] Movimiento personaje ninio y fantasma

---
+++ Sleepwalker/Assets/Scripts/Fantasma.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fantasma : MonoBehaviour {
+
+    public float desplazamiento_movimiento = 9f;
+    public float desplazamiento_salto = 9f;
+    Rigidbody2D rg;
+
+    // Use this for initialization
+    void Start () {
+        rg = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.W)){ //Arriba
+            rg.velocity = new Vector2(0, desplazamiento_salto); //rg.transform.position.x     //Ajustar que el lanzamiento para que no sea tan cortado.
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)){ //Derecha
+            rg.velocity = new Vector2(desplazamiento_movimiento, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A)){ //Izquierda
+            rg.velocity = new Vector2(-desplazamiento_movimiento, 0);
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.Space)){
+
+        }*/
+    }
+}
---
+++ Sleepwalker/Assets/Scripts/Mover_ninio.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover_ninio : MonoBehaviour {
+
+    public float Velocidad = 0.3f; //Se recomienda 0.3 o 0.2
+    Rigidbody2D rg;
+
+    // Use this for initialization
+    void Start () {
+	    rg = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rg.velocity = new Vector2(Velocidad, 0);
+    }
+}
