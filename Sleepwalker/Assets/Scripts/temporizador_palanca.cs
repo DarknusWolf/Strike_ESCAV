ead4acc56ffe51cabdf383c1f84efe48abc96d88
carol555 <carolinaap4d@gmail.com>
2015-11-28 12:16:17 +0100

    [4] Script y funcionamiento de la palanca temporizador

---
+++ Sleepwalker/Assets/Scripts/temporizador_palanca.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+
+public class temporizador_palanca : MonoBehaviour {
+	public float tiempo = 3f;
+	public Animator Target; //meter animacion puerta
+	bool dentro = false;
+	Animator mi_animacion;
+
+	void Start(){
+		mi_animacion = GetComponent<Animator> (); //animacion palanca
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.Space) && dentro){
+			activar();
+			Invoke("desactivar",tiempo);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D objeto){
+		if (objeto.transform.tag == "Player") {
+			dentro = true;
+		}
+	}
+
+
+	void OnTriggerExit2D(Collider2D objeto){
+		dentro = false;
+	}
+
+	void activar(){
+		mi_animacion.SetBool ("Activar",true);
+	
+		Target.SetBool("Activar", true);
+	}
+
+	void desactivar(){
+		mi_animacion.SetBool ("Activar",false);
+		
+		Target.SetBool("Activar", false);
+	}
+}
